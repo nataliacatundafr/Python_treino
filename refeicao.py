dd13b3b7497bde238b3c57b635778ceabceaaa4f
natalicatundafr <ncatundafr@gmail.com>
2020-06-22 20:27:46 -0300

    first commit

---
+++ refeicao.py
@@ -0,0 +1,18 @@
+def almoco(comida_dia):
+    print(comida_dia)
+
+
+comida_dia=input("O que vc comeu de dia:")
+almoco()
+def janta(comida_noite):
+    print(comida_noite)
+
+
+comida_noite = input( "O que vc comeu de dia:" )
+janta()
+
+if comida_noite != comida_dia :
+    print("Você comeu coisa nova!")
+else:
+    print("Você comeu o mesmo do almoço!")
+
